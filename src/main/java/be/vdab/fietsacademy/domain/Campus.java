36bbdc721a338cfd9b0c4f6dd48e0df3f8e75ed6
Matlockyholmes <matthiasheylen1@gmail.com>
2019-06-23 23:40:02 +0200

    "Records wijzigen"

--- src/main/java/be/vdab/fietsacademy/domain/Campus.java
+++ src/main/java/be/vdab/fietsacademy/domain/Campus.java
@@ -2,6 +2,9 @@ package be.vdab.fietsacademy.domain;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 @Entity
 @Table(name="campussen")
@@ -13,6 +16,10 @@ public class Campus implements Serializable {
     private String naam;
     @Embedded
     private Adres adres;
+    @ElementCollection
+    @CollectionTable(name = "campussentelefoonnrs", joinColumns = @JoinColumn(name = "campusId"))
+    @OrderBy("fax")
+    private Set<TelefoonNr> telefoonNrs;
 
     protected Campus() {
     }
@@ -20,6 +27,7 @@ public class Campus implements Serializable {
     public Campus(String naam, Adres adres) {
         this.naam = naam;
         this.adres = adres;
+        this.telefoonNrs = new LinkedHashSet<>();
     }
 
     public long getId() {
@@ -32,5 +40,9 @@ public class Campus implements Serializable {
 
     public Adres getAdres() {
         return adres;
+    }
+
+    public Set<TelefoonNr> getTelefoonNrs(){
+        return Collections.unmodifiableSet(telefoonNrs);
     }
 }
